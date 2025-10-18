73eca4b94e7e9794c9cf25e188aaecc664e28b43
franks toscani <franks.koudjagochui@studio.unibo.it>
2025-10-18 17:34:22 +0200

    riorganizzazione del codice in MVC.

--- src/oracle.js
+++ src/oracle.js
@@ -1,11 +1,13 @@
 const axios = require("axios");
+const config = require("./config");
 
 // Simuliamo una lista di requestId da testare
 const requestIds = ["req-001", "req-002", "req-003", "req-004", "req-005"];
 
 async function fetchMatches(requestId) {
     try {
-        const res = await axios.get(`http://localhost:3000/response/${requestId}`);
+        const url = `http://localhost:${config.http.port}/response/${requestId}`;
+        const res = await axios.get(url);
         console.log(`Risultati per ${requestId}:`);
         console.dir(res.data, { depth: null });
         console.log("\n");
@@ -24,4 +26,10 @@ async function runOracle() {
     }
 }
 
-runOracle();
+if (require.main === module) {
+    runOracle();
+}
+
+module.exports = {
+    runOracle,
+};
